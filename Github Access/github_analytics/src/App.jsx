a2d7a7d87e56971c8b1f285bbc608e45e9cff8dc
ciaraos <47390332+ciaraos@users.noreply.github.com>
2019-11-25 16:44:36 +0000

    updated to call added components

--- Github Access/github_analytics/src/App.jsx
+++ Github Access/github_analytics/src/App.jsx
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Form from './components/Form.jsx';
+import RepoList from './components/RepoList.jsx';
+import GitProfileDetails from './components/GitProfileDetails.jsx';
+import ProgrammingLang from './components/ProgrammingLang.jsx';
+import lda from './lda';
+import Plot from 'react-plotly.js';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Tab from 'react-bootstrap';
+//import Tabs from './components/Tabs.jsx';
+
+import './App.css';
+import './index.css';
+//import './index.html';
+
+import * as d3 from 'd3';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap.css';
+//import './d3.js';
+
+var values = [19, 26, 55];
+var labels = ['Residential', 'Non-Residential', 'Utility'];
+var type = 'pie';
+
 class App extends Component {
+  
   constructor() {
     super();
     this.state = {
       gitun: 'No username',
+      infoclean : '',
       info: '',
       formData: {
         username: '',
-      }
-    }
+      },
+      repitems: null,
+      staritems: null,
+      replanguagecount: {},
+      keywords: null
+}
     this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
     this.handleFormChange= this.handleFormChange.bind(this);
   }
@@ -19,9 +47,65 @@ handleUserFormSubmit(event) {
     axios.get('https://api.github.com/users/'+this.state.formData.username)
     .then(response => this.setState({
       gitun: response.data.login,
+      infoclean: response.data,
       info : JSON.stringify(response.data, undefined, 2)
     })).catch((err) => { console.log(err); });
-  };
+axios.get('https://api.github.com/users/'+this.state.formData.username+'/repos')
+    .then(response => {
+var itemsWithFalseForks = response.data.filter(item => item.fork === false)
+var sortedItems = itemsWithFalseForks.sort((b,a) => {
+        if((a.watchers_count +  a.forks_count) < (b.forks_count + b.watchers_count)){
+          return -1
+        }else if ((a.watchers_count +  a.forks_count) > (b.forks_count + b.watchers_count)){
+          return 1
+        }else {
+          return 0
+        }
+      })
+let dictrlc = Object.assign({}, this.state.replanguagecount);
+      for (var i = 0; i < itemsWithFalseForks.length; i++) {
+          dictrlc[itemsWithFalseForks[i]['language']] = -~ dictrlc[itemsWithFalseForks[i]['language']]
+      }
+this.setState({
+        repitems: sortedItems.slice(0,10),
+        replanguagecount: dictrlc,
+      })
+}).catch((err) => { console.log(err); })
+axios.get('https://api.github.com/users/'+this.state.formData.username+'/starred')
+    .then(response => {
+var itemsWithFalseForks = response.data.filter(item => item.fork === false)
+var sortedItems = itemsWithFalseForks.sort((b,a) => {
+        if((a.watchers_count +  a.forks_count) < (b.forks_count + b.watchers_count)){
+          return -1
+        }else if ((a.watchers_count +  a.forks_count) > (b.forks_count + b.watchers_count)){
+          return 1
+        }else {
+          return 0
+        }
+      })
+var documents = []
+      for (var i = 0; i < response.data.length; i++) {
+          var descr = response.data[i]['description']
+          if (descr != null) {
+            var newtext = descr.match(/[^.!?]+[.!?]+/g)
+            if (newtext != null) {
+              documents = documents.concat(newtext)
+            }
+          }
+      }
+      var result = lda(documents, 3, 3);
+      var keywords = new Set()
+      for (var k = 0; k < 3; k++) {
+        for (var j = 0; j < 3; j++) {
+          keywords = keywords.add(result[k][j]['term']);
+        }
+      }
+this.setState({
+        staritems: sortedItems.slice(0,10),
+        keywords: Array.from(keywords).join(', ')
+      })
+}).catch((err) => { console.log(err); })
+};
 handleFormChange(event) {
     const obj = this.state.formData;
     obj[event.target.name] = event.target.value;
@@ -34,18 +118,30 @@ render() {
           <h1 className="App-title">GitHub Analytics</h1>
         </header>
         <p className="App-intro">
-          Watch this space...
+          Type in a GitHub username to analyse :)
         </p>
+        <hr></hr>
         <Form
           formData={this.state.formData}
           handleUserFormSubmit={this.handleUserFormSubmit}
           handleFormChange={this.handleFormChange}
         />
-        <p><b>Username:</b></p>
-        <p>{this.state.gitun}</p>
-        <b>Information:</b>
-        <pre>{this.state.info}</pre>
-</div>
+        <hr></hr>
+        Profile Details:
+        <GitProfileDetails infoclean={this.state.infoclean}/>
+        <hr></hr>
+        Repositories:
+        <RepoList repitems={this.state.repitems}/>
+        <hr></hr>
+        Starred Repositories:
+        <RepoList repitems={this.state.staritems}/>
+        <hr></hr>
+        Programming Languages:
+        <ProgrammingLang langslist={this.state.replanguagecount}/>
+         <div className='App'>
+      </div>
+      </div>
+      
     );
   }
 }
