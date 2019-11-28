bfe7f1119c311a3a67a374b5457b61b120a0d715
Ciara O'Sullivan <47390332+ciaraos@users.noreply.github.com>
2019-11-28 12:12:38 +0000

    added code to call chart

--- Github Access/github_analytics/src/components/ProgrammingLang.jsx
+++ Github Access/github_analytics/src/components/ProgrammingLang.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
+//import BarChart from './d3.jsx';
+import BarChart from './BarChart.jsx';
+
+let x = ''
+let i = ''
+
+
+
 const LanguageList = (props) => {
-  if (props.langslist) {
-    return (
-        <ul>
-          {Object.entries(props.langslist).map(([key,value]) =>
-            <li key={key}>
-              {key} - {value}
-            </li>
-          )}
-        </ul>
-      )
-  } else { return null;}
-  };
+    if (props.langslist) {
+        return (
+            <ul>
+                {Object.entries(props.langslist).map(([key, value]) =>
+                    <li key={key}>
+                        {key} - {value}
+                        {console.log(x = x + key+' ')}
+                        {console.log(i = i + value)}
+                        
+
+
+                    </li>
+                )}
+                <div>
+                <BarChart repoNames={x.split(' ')} data={i.split('')}/>
+                    {i = ''}
+                    {x=''}
+                </div>
+            </ul>
+        )
+    } else { return null }
+};
 export default LanguageList;
